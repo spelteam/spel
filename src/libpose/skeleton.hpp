6bdcaeaafefa1e1e5c29ec2dcffa830dc80170ee
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2014-07-09 09:13:17 +0000

    <Vitaliy Koshura>: Fix

--- src/libpose/skeleton.hpp
+++ src/libpose/skeleton.hpp
@@ -1,8 +1,16 @@
 #ifndef _LIBPOSE_SKELETON_HPP_
 #define _LIBPOSE_SKELETON_HPP_
 
+#include <string>
+
+using namespace std;
+
 class Skeleton
 {
+  public:
+  private:
+    string name;
+    
 };
 
 #endif  // _LIBPOSE_SKELETON_HPP_
