b6cf1ad54f40ac3709fdf36f8196d9a141c2cd45
mfastovets <mfastovets@users.noreply.github.com>
2014-07-11 02:24:09 +0100

    <Mykyta Fastovets> Updated UML. Added solution.hpp, solution.cpp. Added fromXml function to Skelton.

---
+++ src/libpose/solution.hpp
@@ -0,0 +1,36 @@
+#ifndef _SOLUTION_HPP_
+#define _SOLUTION_HPP_
+
+#include <string>
+#include <vector>
+#include "limbLabel.hpp"
+
+using namespace std;
+
+class Score
+{
+  public:
+    Solution(void);
+    Solution(int id, string sName, vector<float> params, vector<LimbLabel> sol);
+    
+    Solution & operator=(const Solution &s);
+    bool operator==(const Solution &s) const;
+    bool operator!=(const Solution &s) const;
+
+    int getId(void);
+    void setId(int _id);
+
+    vector<LimbLabel> getLabels(void);
+    void setLabels(vector<LimbLabel> _labels);
+
+    Skeleton toSkeleton(void);
+  
+  private:
+    int solverId; //id of the solver solution was obtained from
+    string solverName: //name of the solver
+    vector<float> solverParams;
+    vector<LimbLabel> labels;
+};
+
+#endif  // _SOLUTION_HPP_
+ 
