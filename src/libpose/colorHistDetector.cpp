989020cc13c09ea160b7ccaa8f177ab8bbd9ddc1
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2014-07-24 13:18:21 +0000

    <Vitaliy Koshura>: Refactoring code. We will work with multiple partModels so we need to move some fields to the separated struct inside our class.

--- src/libpose/colorHistDetector.cpp
+++ src/libpose/colorHistDetector.cpp
@@ -1,7 +1,7 @@
 #include "colorHistDetector.hpp"
 
 //TODO (Vitaliy Koshura): Need unit test
-ColorHistDetector::ColorHistDetector(uint8_t _nBins) : nBins(_nBins)
+ColorHistDetector::PartModel::PartModel(uint8_t _nBins) : nBins(_nBins)
 {
   partHistogramm.resize(nBins);
   for (uint8_t r = 0; r < nBins; r++)
@@ -18,6 +18,11 @@ ColorHistDetector::ColorHistDetector(uint8_t _nBins) : nBins(_nBins)
   }
   sizeFG = 0;
   uniqueExists = false;
+}
+
+//TODO (Vitaliy Koshura): Need unit test
+ColorHistDetector::ColorHistDetector(uint8_t _nBins) : nBins(_nBins)
+{
 }
 
 int ColorHistDetector::getID(void)
@@ -48,34 +53,34 @@ uint8_t ColorHistDetector::getNBins(void)
 }
 
 //TODO (Vitaliy Koshura): Need unit test
-float ColorHistDetector::computePixelBelongingLikelihood(uint8_t r, uint8_t g, uint8_t b)
+float ColorHistDetector::computePixelBelongingLikelihood(uint32_t nPartModel, uint8_t r, uint8_t g, uint8_t b)
 {
-  uint8_t factor = ceil(pow(2, 8)/getNBins());
-  float isFG = partHistogramm[r/factor][g/factor][b/factor];
+  uint8_t factor = ceil(pow(2, 8)/partModels.at(nPartModel).nBins);
+  float isFG = partModels.at(nPartModel).partHistogramm[r/factor][g/factor][b/factor];
   return isFG;
 }
 
 //TODO (Vitaliy Koshura>: need unit test
-void ColorHistDetector::setPartHistogramm(const vector <Point3i> &partColors)
+void ColorHistDetector::setPartHistogramm(uint32_t nPartModel, const vector <Point3i> &partColors)
 {
   // do not add sample if the number of pixels is zero
   if (partColors.size() == 0)
     return;
-  uniqueExists = false;
-  uint8_t factor = ceil(pow(2, 8)/getNBins());  // divide the color space into bins
-  sizeFG = partColors.size();
-  fgNumSamples = 1;
-  fgSampleSizes.clear();
-  fgSampleSizes.push_back(partColors.size());
+  partModels.at(nPartModel).uniqueExists = false;
+  uint8_t factor = ceil(pow(2, 8)/partModels.at(nPartModel).nBins);  // divide the color space into bins
+  partModels.at(nPartModel).sizeFG = partColors.size();
+  partModels.at(nPartModel).fgNumSamples = 1;
+  partModels.at(nPartModel).fgSampleSizes.clear();
+  partModels.at(nPartModel).fgSampleSizes.push_back(partColors.size());
 
 // clear histogram first
-  for(uint8_t r = 0; r < getNBins(); r++)
+  for(uint8_t r = 0; r < partModels.at(nPartModel).nBins; r++)
   {
-    for(uint8_t g = 0; g < getNBins(); g++)
+    for(uint8_t g = 0; g < partModels.at(nPartModel).nBins; g++)
     {
-      for(uint8_t b = 0; b < getNBins(); b++)
+      for(uint8_t b = 0; b < partModels.at(nPartModel).nBins; b++)
       {
-        partHistogramm[r][g][b] = 0.0;
+        partModels.at(nPartModel).partHistogramm[r][g][b] = 0.0;
       }
     }
   }
@@ -84,82 +89,82 @@ void ColorHistDetector::setPartHistogramm(const vector <Point3i> &partColors)
     uint8_t r = partColors[i].x / factor;
     uint8_t g = partColors[i].y / factor;
     uint8_t b = partColors[i].z / factor;
-    partHistogramm[r][g][b]++;
+    partModels.at(nPartModel).partHistogramm[r][g][b]++;
   }
-  for(uint8_t r = 0; r < getNBins(); r++)
+  for(uint8_t r = 0; r < partModels.at(nPartModel).nBins; r++)
   {
-    for(uint8_t g = 0; g < getNBins(); g++)
+    for(uint8_t g = 0; g < partModels.at(nPartModel).nBins; g++)
     {
-      for(uint8_t b = 0; b < getNBins(); b++)
+      for(uint8_t b = 0; b < partModels.at(nPartModel).nBins; b++)
       {
 // normalise the histograms
-        partHistogramm[r][g][b] /= sizeFG;
+        partModels.at(nPartModel).partHistogramm[r][g][b] /= partModels.at(nPartModel).sizeFG;
       }
     }
   }
 }
 
 //TODO (Vitaliy Koshura>: need unit test
-void ColorHistDetector::addPartHistogramm(const vector <Point3i> &partColors, uint32_t nBlankPixels)
+void ColorHistDetector::addPartHistogramm(uint32_t nPartModel, const vector <Point3i> &partColors, uint32_t nBlankPixels)
 {
   if(partColors.size()==0) //do not add sample if the number of pixels is zero
     return;
-  uniqueExists = false;
+  partModels.at(nPartModel).uniqueExists = false;
 //un-normalise
-  for(uint8_t r = 0; r < getNBins(); r++)
+  for(uint8_t r = 0; r < partModels.at(nPartModel).nBins; r++)
   {
-    for(uint8_t g = 0; g < getNBins(); g++)
+    for(uint8_t g = 0; g < partModels.at(nPartModel).nBins; g++)
     {
-      for(uint8_t b = 0; b < getNBins(); b++)
+      for(uint8_t b = 0; b < partModels.at(nPartModel).nBins; b++)
       {
-        partHistogramm[r][g][b] *= sizeFG;
+        partModels.at(nPartModel).partHistogramm[r][g][b] *= partModels.at(nPartModel).sizeFG;
       }
     }
   }
 
-  int factor = ceil(pow(2, 8)/getNBins());//divide the color space into bins
-  sizeFG += partColors.size();
-  fgNumSamples++;
-  fgSampleSizes.push_back(partColors.size());
+  int factor = ceil(pow(2, 8)/partModels.at(nPartModel).nBins);//divide the color space into bins
+  partModels.at(nPartModel).sizeFG += partColors.size();
+  partModels.at(nPartModel).fgNumSamples++;
+  partModels.at(nPartModel).fgSampleSizes.push_back(partColors.size());
 
   for(int i = 0; i < partColors.size(); i++)
   {
     uint8_t r = partColors[i].x / factor;
     uint8_t g = partColors[i].y / factor;
     uint8_t b = partColors[i].z / factor;
-    partHistogramm[r][g][b]++;
+    partModels.at(nPartModel).partHistogramm[r][g][b]++;
   }
 
 //renormalise
-  for(uint8_t r = 0; r < getNBins(); r++)
+  for(uint8_t r = 0; r < partModels.at(nPartModel).nBins; r++)
   {
-    for(uint8_t g = 0; g < getNBins(); g++)
+    for(uint8_t g = 0; g < partModels.at(nPartModel).nBins; g++)
     {
-      for(uint8_t b = 0; b < getNBins(); b++)
+      for(uint8_t b = 0; b < partModels.at(nPartModel).nBins; b++)
       {
 //normalise the histograms
-        partHistogramm[r][g][b] /= sizeFG;
+        partModels.at(nPartModel).partHistogramm[r][g][b] /= partModels.at(nPartModel).sizeFG;
       }
     }
   }
 
-  fgBlankSizes.push_back(nBlankPixels); //add the number of blank pixels for this model
+  partModels.at(nPartModel).fgBlankSizes.push_back(nBlankPixels); //add the number of blank pixels for this model
 }
 
-float ColorHistDetector::getAvgSampleSizeFg(void)
+float ColorHistDetector::getAvgSampleSizeFg(uint32_t nPartModel)
 {
   float sum = 0;
-  for(uint32_t i = 0; i < fgSampleSizes.size(); i++)
+  for(uint32_t i = 0; i < partModels.at(nPartModel).fgSampleSizes.size(); i++)
   {
-    sum += fgSampleSizes[i];
+    sum += partModels.at(nPartModel).fgSampleSizes[i];
   }
-  sum /= fgNumSamples;
+  sum /= partModels.at(nPartModel).fgNumSamples;
   return sum;
 }
 
-float ColorHistDetector::getAvgSampleSizeFgBetween(uint32_t s1, uint32_t s2)
+float ColorHistDetector::getAvgSampleSizeFgBetween(uint32_t nPartModel, uint32_t s1, uint32_t s2)
 {
-  if(s1 >= fgSampleSizes.size() || s2 >= fgSampleSizes.size())
+  if(s1 >= partModels.at(nPartModel).fgSampleSizes.size() || s2 >= partModels.at(nPartModel).fgSampleSizes.size())
     return 0;
-  return (fgSampleSizes[s1] + fgSampleSizes[s2]) / 2.0;
+  return (partModels.at(nPartModel).fgSampleSizes[s1] + partModels.at(nPartModel).fgSampleSizes[s2]) / 2.0;
 }
--- src/libpose/colorHistDetector.hpp
+++ src/libpose/colorHistDetector.hpp
@@ -18,20 +18,25 @@ class ColorHistDetector : public Detector
     vector <vector <LimbLabel> > detect(Frame *frame, vector <float> params);
     uint8_t getNBins(void);
   private:
+    struct PartModel
+    {
+      PartModel(uint8_t _nBins);
+      const uint8_t nBins;
+      vector <vector <vector <float>>> partHistogramm;
+      uint32_t sizeFG;
+      bool uniqueExists;
+      uint32_t fgNumSamples;
+      vector <uint32_t> fgSampleSizes;
+      vector <uint32_t> fgBlankSizes;
+    };
     int id;
     const uint8_t nBins;
-    vector <vector <vector <float>>> partHistogramm;
-    uint32_t sizeFG;
-    bool uniqueExists;
-    uint32_t fgNumSamples;
-    vector <uint32_t> fgSampleSizes;
-    vector <uint32_t> fgBlankSizes;
-
-    float computePixelBelongingLikelihood(uint8_t r, uint8_t g, uint8_t b);
-    void setPartHistogramm(const vector <Point3i> &partColors);
-    void addPartHistogramm(const vector <Point3i> &partColors, uint32_t nBlankPixels);
-    float getAvgSampleSizeFg(void);
-    float getAvgSampleSizeFgBetween(uint32_t s1, uint32_t s2);
+    vector <PartModel> partModels;
+    float computePixelBelongingLikelihood(uint32_t nPartModel, uint8_t r, uint8_t g, uint8_t b);
+    void setPartHistogramm(uint32_t nPartModel, const vector <Point3i> &partColors);
+    void addPartHistogramm(uint32_t nPartModel, const vector <Point3i> &partColors, uint32_t nBlankPixels);
+    float getAvgSampleSizeFg(uint32_t nPartModel);
+    float getAvgSampleSizeFgBetween(uint32_t nPartModel, uint32_t s1, uint32_t s2);
 };
 
 #endif  // _LIBPOSE_COLORHISTDETECTOR_HPP_
