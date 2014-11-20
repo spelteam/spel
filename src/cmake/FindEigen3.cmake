3516463d8b7eae53b29d1c2d4882907d1c6a6c94
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2014-11-20 22:27:54 +0000

    <Vitaliy Koshura>: Fix issue #236: Add Eigen library to project: Linux version

---
+++ src/cmake/FindEigen3.cmake
@@ -0,0 +1,4 @@
+FIND_PATH ( Eigen3_INCLUDE_DIR eigen3/signature_of_eigen3_matrix_library )
+INCLUDE ( FindPackageHandleStandardArgs )
+FIND_PACKAGE_HANDLE_STANDARD_ARGS ( Eigen3 DEFAULT_MSG Eigen3_INCLUDE_DIR )
+
