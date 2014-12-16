f928cbcbba2707f082db073e39efcb6559b9bbe9
Andrij <makedonsky94@gmail.com>
2014-12-16 15:33:44 +0200

    Create general tests for class derived from Frame
    -test all Frame functionality in 'frame_general_tests.cpp'
    
    -remove the same tests from interpolation, keyframe and lockframe
    tests files.

--- src/tests/libpose/keyframe_get_and_set_tests.cpp
+++ src/tests/libpose/keyframe_get_and_set_tests.cpp
@@ -1,43 +1,20 @@
 #include <gtest/gtest.h>
 #include <keyframe.hpp>
-TEST(keyframeTest, GetAndSetTest)
-{
-  int id = 7;
-  Mat image = Mat::zeros(100, 100, CV_32F);
-  Point pt1, pt2;
-  pt1.x = 10;
-  pt1.y = 10;
-  pt2.x = 90;
-  pt2.y = 90;
-  int icolor = 0x00FFAA11;
-  Scalar color(icolor&255, (icolor>>8)&255, (icolor>>16)&255);
-  line(image, pt1, pt2, color, 5, 8);
 
-  Mat mask = Mat::zeros(100, 100, CV_32F);
-  pt1.x = 10;
-  pt1.y = 90;
-  pt2.x = 90;
-  pt2.y = 10;
-  icolor = 0x00AABBCC;
-  Scalar color1(icolor&255, (icolor>>8)&255, (icolor>>16)&255);
-  line(mask, pt1, pt2, color1, 9, 8);
+class KeyframeTest : public testing::Test{
+protected:
+    //init
+    //nothing to init here
+    /*virtual void SetUp(){}*/
 
-  Skeleton skeleton;
-  Point2f groundPoint(0.0, 0.0);
+    //clear
+    //nothing to clear here
+    /*virtual void TearDown(){}*/
+protected:
+    Keyframe keyframe;
+};
 
-  Keyframe keyframe;
-  keyframe.setID(id);
-  keyframe.setImage(image);
-  keyframe.setMask(mask);
-  keyframe.setSkeleton(skeleton);
-  keyframe.setGroundPoint(groundPoint);
-
-  EXPECT_EQ(id, keyframe.getID());
-  EXPECT_EQ(0, countNonZero(image != keyframe.getImage()));
-  EXPECT_EQ(0, countNonZero(mask != keyframe.getMask()));
-  EXPECT_EQ(skeleton, keyframe.getSkeleton());
-  EXPECT_EQ(groundPoint, keyframe.getGroundPoint());
-
-  EXPECT_EQ(KEYFRAME, keyframe.getFrametype());
+TEST_F( KeyframeTest, GetFrametype ){
+    EXPECT_EQ( FRAMETYPE::KEYFRAME, keyframe.getFrametype() );
 }
 
--- src/tests/libpose/lockframe_get_and_set_tests.cpp
+++ src/tests/libpose/lockframe_get_and_set_tests.cpp
@@ -4,84 +4,15 @@
 class LockframeTest : public testing::Test{
 protected:
     //init
-    virtual void SetUp(){
-        //set id
-        id = 7;
-
-        //set image
-        Point pt1, pt2;
-        pt1.x = 10;
-        pt1.y = 10;
-        pt2.x = 90;
-        pt2.y = 90;
-
-        int icolor = 0x00FFAA11;
-        Scalar color_image(icolor&255, (icolor>>8)&255, (icolor>>16)&255);
-
-        image = Mat::zeros(100,100, CV_32F);
-        line(image, pt1, pt2, color_image, 5, 8);
-
-        //set mask
-        pt1.x = 10;
-        pt1.y = 90;
-        pt2.x = 90;
-        pt2.y = 10;
-
-        icolor = 0x00AABBCC;
-        Scalar color_mask(icolor&255, (icolor>>8)&255, (icolor>>16)&255);
-
-        mask = Mat::zeros(100, 100, CV_32F);
-        line(mask, pt1, pt2, color_mask, 9, 8);
-
-        //set skeleton
-        //nothing
-
-        //set groupPoint
-        groundPoint = Point2f(0.0,0.0);
-
-        //create lockframe
-        lockframe.setID(id);
-        lockframe.setImage(image);
-        lockframe.setMask(mask);
-        lockframe.setSkeleton(skeleton);
-        lockframe.setGroundPoint(groundPoint);
-
-    }
+    //nothing to init here
+    /*virtual void SetUp(){}*/
 
     //clear
     //nothing to clear here
     /*virtual void TearDown(){}*/
 protected:
-    int id;
-    Mat image;
-    Mat mask;
-    Skeleton skeleton;
-    Point2f groundPoint;
-
     Lockframe lockframe;
 };
-
-TEST_F(LockframeTest, GetId){
-    EXPECT_EQ( id, lockframe.getID() );
-}
-
-TEST_F( LockframeTest, GetImage ){
-    EXPECT_EQ( 0, cv::countNonZero(image != lockframe.getImage()) );
-    EXPECT_EQ( image.rows*image.cols, cv::countNonZero(image == lockframe.getImage()) );
-}
-
-TEST_F( LockframeTest, GetMask ){
-    EXPECT_EQ( 0, cv::countNonZero(mask != lockframe.getMask()) );
-    EXPECT_EQ( mask.rows*mask.cols, cv::countNonZero(mask == lockframe.getMask()) );
-}
-
-TEST_F( LockframeTest, GetSkeleton ){
-    EXPECT_EQ( skeleton, lockframe.getSkeleton() );
-}
-
-TEST_F( LockframeTest, GetGroundPoint ){
-    EXPECT_EQ( groundPoint, lockframe.getGroundPoint() );
-}
 
 TEST_F( LockframeTest, GetFrametype ){
     EXPECT_EQ( FRAMETYPE::LOCKFRAME, lockframe.getFrametype() );
