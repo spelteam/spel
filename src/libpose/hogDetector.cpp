5b8dec6c3f79b7e384c7bbff2805d7ea92250e26
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2015-01-06 03:35:41 +0200

    <Vitalii Koshura>: Small refactoring

--- src/libpose/hogDetector.cpp
+++ src/libpose/hogDetector.cpp
@@ -1,5 +1,7 @@
 #include "hogDetector.hpp"
 
+#define ERROR_HEADER __FILE__ << ":" << __LINE__ << ": "
+
 int HogDetector::getID(void)
 {
   return id;
@@ -8,6 +10,131 @@ int HogDetector::getID(void)
 void HogDetector::setID(int _id)
 {
   id = _id;
+}
+
+map <PHPoint<uint32_t>, vector <float>> HogDetector::computeDescriptors(HOGDescriptor detector, Size wndSize, Size wndStride, Size blockSize, Size blockStride, Size cellSize, int nbins, Frame *frame)
+{
+  uint64_t d = 0;
+  uint32_t i = 0, j = 0, n = 0, k = 0, r = 0, c = 0, b = 0, row = 0, col = 0;
+  Mat img = frame->getImage();
+
+  vector <float> descriptors;
+
+  detector.compute(img, descriptors);
+
+#ifdef DEBUG
+  cout << "Decriptors: " << descriptors.size() << endl;
+#endif  // DEBUG
+
+  map <PHPoint<uint32_t>, vector <float>> currentFrameRawDescriptors;
+
+  for (uint32_t row = 0; row < img.rows; row++)
+  {
+    for (uint32_t col = 0; col < img.cols; col++)
+    {
+      currentFrameRawDescriptors.insert(pair <PHPoint<uint32_t>, vector <float>>(PHPoint<uint32_t>(row, col), vector <float>()));
+    }
+  }
+
+  try
+  {
+    // image rows
+    for (i = 0; i + wndSize.height < img.rows + wndStride.height; i += wndStride.height)
+    {
+      // image cols
+      for (j = 0; j + wndSize.width < img.cols + wndStride.width; j += wndStride.width)
+      {
+        // window rows
+        for (n = i; n + blockStride.height < i + wndSize.height; n += blockStride.height)
+        {
+          // window cols
+          for (k = j; k + blockStride.width < j + wndSize.width; k += blockStride.width)
+          {
+            // block rows
+            for (r = n; r < n + blockSize.height; r += cellSize.height)
+            {
+              // block cols
+              for (c = k; c < k + blockSize.width; c += cellSize.width)
+              {
+                // nbins
+                for (b = 0; b < nbins; b++)
+                {
+                  // cell rows
+                  for (row = r; row < cellSize.height; row++)
+                  {
+                    // cell cols
+                    for (col = c; col < cellSize.width; col++)
+                    {
+                      currentFrameRawDescriptors[PHPoint<uint32_t>(row, col)].push_back(descriptors.at(d));
+                    }
+                  }
+                  d++;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  catch (...)
+  {
+    stringstream ss;
+    ss << "Descriptor parse error:" << endl << "Image row:\t" << i << "\tImage col:\t" << j << endl << "Window row:\t" << n << "\tWindow col:\t" << k << endl << "Block row:\t" << r << "\tBlock col:\t" << c << endl << "NBins:\t" << b << endl;
+    ss << "Total image rows:\t" << img.rows << "\tTotal image cols:\t" << img.cols << endl;
+    ss << "Total descriptors:\t" << descriptors.size() << endl;
+    ss << "Trying to get Point at:\t" << row << ":" << col << endl;
+    ss << "Trying to get descriptor at:\t" << d << endl;
+#ifdef DEBUG
+    cerr << ERROR_HEADER << ss.str() << endl;
+#endif // DEBUG
+    throw logic_error(ss.str());
+  }
+
+  if (d < descriptors.size())
+  {
+    stringstream ss;
+    ss << "Error. Not all descriptors were parsed" << endl;
+    ss << "Last parsed descriptor:\t" << d << endl;
+    ss << "Total descriptor count:\t" << descriptors.size() << endl;
+#ifdef DEBUG
+    cerr << ERROR_HEADER << ss.str() << endl;
+#endif // DEBUG
+    throw logic_error(ss.str());
+  }
+
+  rawDescriptors.insert(pair <uint32_t, map <PHPoint<uint32_t>, vector <float>>>(frame->getID(), currentFrameRawDescriptors));
+  return currentFrameRawDescriptors;
+}
+
+void HogDetector::parseBodyPartDescriptors(Frame *frame, map <PHPoint<uint32_t>, vector <float>> currentFrameRawDescriptors)
+{
+
+  tree <BodyPart> trBodyPart = frame->getSkeleton().getPartTree();
+  map <uint32_t, map<PHPoint<float>, vector <float>>> frameDescriptors;
+  for (tree <BodyPart>::iterator part = trBodyPart.begin(); part != trBodyPart.end(); ++part)
+  {
+    map <PHPoint <float>, vector <float>> partMap;
+    POSERECT<Point2f> rect = part->getPartPolygon();
+    float xmax, ymax, xmin, ymin;
+    rect.GetMinMaxXY <float>(xmin, ymin, xmax, ymax);
+
+    for (uint32_t x = (uint32_t)xmin; x <= (uint32_t)xmax; x++)
+    {
+      for (uint32_t y = (uint32_t)ymin; y <= (uint32_t)ymax; y++)
+      {
+        Point2f point((float)x, (float)y);
+        PHPoint<uint32_t> phpoint(x, y);
+        if (rect.containsPoint(point))
+        {
+          partMap.insert(pair <PHPoint<float>, vector<float>>(PHPoint<float>((float)x, (float)y), currentFrameRawDescriptors.at(phpoint)));
+        }
+      }
+    }
+    frameDescriptors.insert(pair <uint32_t, map <PHPoint<float>, vector<float>>>(part->getPartID(), partMap));
+  }
+
+  frameBodyPartDescriptors.insert(pair <uint32_t, map <uint32_t, map<PHPoint<float>, vector <float>>>>(frame->getID(), frameDescriptors));
 }
 
 //TODO (Vitaliy Koshura): Write real implementation here
@@ -43,114 +170,18 @@ void HogDetector::train(vector <Frame*> frames, map <string, float> params)
 #endif  // DEBUG
     vector <Rect> found;
     vector <Rect> found_filtered;
-    Mat img = (*frameNum)->getImage();
-
-    vector <float> descriptors;
-
-    detector.compute(img, descriptors);
-
-#ifdef DEBUG
-    cout << "Decriptors: " << descriptors.size() << endl;
-#endif  // DEBUG
-
-    map <PHPoint<uint32_t>, vector <float>> currentFrameRawDescriptors;
-    for (uint32_t row = 0; row < img.rows; row++)
-    {
-      for (uint32_t col = 0; col < img.cols; col++)
-      {
-        currentFrameRawDescriptors.insert(pair <PHPoint<uint32_t>, vector <float>>(PHPoint<uint32_t>(row, col), vector <float>()));
-      }
-    }
-    uint64_t d = 0;
-    uint32_t i = 0, j = 0, n = 0, k = 0, r = 0, c = 0, b = 0, row = 0, col = 0;
 
     try
     {
-// image rows
-      for (i = 0; i + wndSize.height < img.rows + wndStride.height; i += wndStride.height)
-      {
-// image cols
-        for (j = 0; j + wndSize.width < img.cols + wndStride.width; j += wndStride.width)
-        {
-// window rows
-          for (n = i; n + blockStride.height < i + wndSize.height; n += blockStride.height)
-          {
-// window cols
-            for (k = j; k + blockStride.width < j + wndSize.width; k += blockStride.width)
-            {
-// block rows
-              for (r = n; r < n + blockSize.height; r += cellSize.height)
-              {
-// block cols
-                for (c = k; c < k + blockSize.width; c += cellSize.width)
-                {
-// nbins
-                  for (b = 0; b < nbins; b++)
-                  {
-// cell rows
-                    for (row = r; row < cellSize.height; row++)
-                    {
-// cell cols
-                      for (col = c; col < cellSize.width; col++)
-                      {
-                        currentFrameRawDescriptors[PHPoint<uint32_t>(row, col)].push_back(descriptors.at(d));
-                      }
-                    }
-                    d++;
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
+      parseBodyPartDescriptors(*frameNum, computeDescriptors(detector, wndSize, wndStride, blockSize, blockStride, cellSize, nbins, *frameNum));
     }
     catch (...)
     {
-      cerr << "Descriptor parse error:" << endl << "Image row:\t" << i << "\tImage col:\t" << j << endl << "Window row:\t" << n << "\tWindow col:\t" << k << endl << "Block row:\t" << r << "\tBlock col:\t" << c << endl << "NBins:\t" << b << endl;
-      cerr << "Total image rows:\t" << img.rows << "\tTotal image cols:\t" << img.cols << endl;
-      cerr << "Total descriptors:\t" << descriptors.size() << endl;
-      cerr << "Trying to get Point at:\t" << row << ":" << col << endl;
-      cerr << "Trying to get descriptor at:\t" << d << endl;
       break;
     }
 
-    if (d < descriptors.size())
-    {
-      cerr << "Error. Not all descriptors were parsed" << endl;
-      cerr << "Last parsed descriptor:\t" << d << endl;
-      cerr << "Total descriptor count:\t" << descriptors.size() << endl;
-      break;
-    }
-
-    rawDescriptors.insert(pair <uint32_t, map <PHPoint<uint32_t>, vector <float>>>((*frameNum)->getID(), currentFrameRawDescriptors));
-
-    tree <BodyPart> trBodyPart = (*frameNum)->getSkeleton().getPartTree();
-    map <uint32_t, map<PHPoint<float>, vector <float>>> frameDescriptors;
-    for (tree <BodyPart>::iterator part = trBodyPart.begin(); part != trBodyPart.end(); ++part)
-    {
-      map <PHPoint <float>, vector <float>> partMap;
-      POSERECT<Point2f> rect = part->getPartPolygon();      
-      float xmax, ymax, xmin, ymin;
-      rect.GetMinMaxXY <float> (xmin, ymin, xmax, ymax);
-
-      for (uint32_t x = (uint32_t)xmin; x <= (uint32_t)xmax; x++)
-      {
-        for (uint32_t y = (uint32_t)ymin; y <= (uint32_t)ymax; y++)
-        {
-          Point2f point((float)x, (float)y);
-          PHPoint<uint32_t> phpoint(x, y);
-          if (rect.containsPoint(point))
-          {
-            partMap.insert(pair <PHPoint<float>, vector<float>>(PHPoint<float>((float)x, (float)y), currentFrameRawDescriptors.at(phpoint)));
-          }
-        }
-      }
-      frameDescriptors.insert(pair <uint32_t, map <PHPoint<float>, vector<float>>>(part->getPartID(), partMap));
-    }
+    /*Mat img = (*frameNum)->getImage();
     
-    frameBodyPartDescriptors.insert(pair <uint32_t, map <uint32_t, map<PHPoint<float>, vector <float>>>>((*frameNum)->getID(), frameDescriptors));
-
     detector.detectMultiScale(img, found, hitThreshold, wndStride, padding, scale0, groupThreshold);
     for (size_t n = 0; n < found.size(); n++)
     {
@@ -166,7 +197,7 @@ void HogDetector::train(vector <Frame*> frames, map <string, float> params)
           found_filtered.push_back(r);
         }
       }
-    }
+    }*/
   }
 }
 
@@ -191,10 +222,12 @@ vector <vector <LimbLabel> > HogDetector::detect(Frame *frame, map <string, floa
 
   vector <Rect> found;
   vector <Rect> found_filtered;
-  Mat img = frame->getImage();
+  //Mat img = frame->getImage();
   HOGDescriptor detector(wndSize, blockSize, blockStride, cellSize, nbins, wndSigma, thresholdL2hys, gammaCorrection, nlevels);
+
+  computeDescriptors(detector, wndSize, wndStride, blockSize, blockStride, cellSize, nbins, frame);
   
-  detector.detectMultiScale(img, found, hitThreshold, wndStride, padding, scale0, groupThreshold);
+  /*detector.detectMultiScale(img, found, hitThreshold, wndStride, padding, scale0, groupThreshold);
   for (size_t n = 0; n < found.size(); n++)
   {
     Rect r = found[n];
@@ -209,7 +242,7 @@ vector <vector <LimbLabel> > HogDetector::detect(Frame *frame, map <string, floa
         found_filtered.push_back(r);
       }
     }
-  }
+  }*/
   vector <vector <LimbLabel> > t;
   return t;
 }
--- src/libpose/hogDetector.hpp
+++ src/libpose/hogDetector.hpp
@@ -16,9 +16,13 @@ class HogDetector : public Detector
     vector <vector <LimbLabel>> detect(Frame *frame, map <string, float> params);
   private:
     int id;
-    map <uint32_t, vector <float>> frameHOGDescriptors;
+    //map <uint32_t, vector <float>> frameHOGDescriptors;
     map <uint32_t, map <PHPoint<uint32_t>, vector <float>>> rawDescriptors;
     map <uint32_t, map <uint32_t, map<PHPoint<float>, vector <float>>>> frameBodyPartDescriptors;
+
+    map <PHPoint<uint32_t>, vector <float>> computeDescriptors(HOGDescriptor detector, Size wndSize, Size wndStride, Size blockSize, Size blockStride, Size cellSize, int nbins, Frame *frame);
+    void parseBodyPartDescriptors(Frame *frame, map <PHPoint<uint32_t>, vector <float>> currentFrameRawDescriptors);
+      
 };
 
 #endif  // _LIBPOSE_HOGDETECTOR_HPP_
