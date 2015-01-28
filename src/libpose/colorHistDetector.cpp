6e71d3b1d33e9a5edd0d15906a64d06e0574d5d9
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2015-01-28 02:06:19 +0200

    <Vitalii Koshura>: Small clean-up

--- src/libpose/colorHistDetector.cpp
+++ src/libpose/colorHistDetector.cpp
@@ -463,60 +463,6 @@ vector <vector <LimbLabel> > ColorHistDetector::detect(Frame *frame, map <string
   uint32_t stepCount = 0;
   uint32_t step = 0;
   getNeighborFrame(&frame, &prevFrame, &nextFrame, step, stepCount);
-  /*for (vector <Frame*>::iterator i = frames.begin(); i != frames.end(); ++i)
-  {
-    Frame *f = *i;
-    if (f->getID() < frame->getID())
-    {
-      if (f->getFrametype() == KEYFRAME || f->getFrametype() == LOCKFRAME)
-      {
-        prevFrame = f;
-        stepCount = 0;
-      }
-      else
-      {
-        stepCount++;
-      }
-    }
-    else if (f->getID() > frame->getID())
-    {
-      stepCount++;
-      if (f->getFrametype() == KEYFRAME || f->getFrametype() == LOCKFRAME)
-      {
-        nextFrame = f;
-        break;
-      }
-    }
-    else // equal
-    {
-      stepCount++;
-      if (prevFrame == 0)
-      {
-        stringstream ss;
-        ss << "Couldn't find previous keyframe to the frame " << frame->getID();
-#ifdef DEBUG
-        cerr << ERROR_HEADER << ss.str() << endl;
-#endif  // DEBUG
-        throw logic_error(ss.str());
-      }
-      else
-      {
-        if (stepCount == 0)
-        {
-          stringstream ss;
-          ss << "Invalid stepCount";
-#ifdef DEBUG
-          cerr << ERROR_HEADER << ss.str() << endl;
-#endif // DEBUG
-          throw logic_error(ss.str());
-        }
-        else
-        {
-          step = stepCount;
-        }
-      }
-    }
-  }*/
   if (prevFrame == 0)
   {
     stringstream ss;
