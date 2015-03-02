1d99a2ce559242d47c762ce8388dcb3abbb26f9f
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2015-03-02 04:05:47 +0200

    <Vitalii Koshura>: Add infer3d test

---
+++ src/tests/libpose/skeleton_infer_tests.cpp
@@ -0,0 +1,163 @@
+#include <gtest/gtest.h>
+#include <skeleton.hpp>
+#include <projectLoader.hpp>
+
+#ifdef WINDOWS
+#include <direct.h>
+#define GetCurrentDir _getcwd
+#else
+#include <unistd.h>
+#define GetCurrentDir getcwd
+#endif
+
+TEST(skeletonTest, infer3dTest)
+{
+  string curFolder;
+  char cCurrentPath[FILENAME_MAX];
+
+  if (!GetCurrentDir(cCurrentPath, sizeof(cCurrentPath)))
+  {
+    FAIL();
+  }
+  curFolder = cCurrentPath;
+  if (curFolder.back() != '/')
+  {
+    curFolder += '/';
+  }
+#ifdef WINDOWS
+  curFolder += "Debug/";
+#endif
+  curFolder += "posetests_TestData/testdata1/";
+
+  ProjectLoader projectLoader(curFolder);
+
+  string xml = curFolder + "trijumpSD_new.xml";
+
+  if (projectLoader.Load(xml) == false)
+  {
+    FAIL();
+  }
+
+  vector <Frame*> frames = projectLoader.getFrames();
+
+  for (vector <Frame*>::iterator frame = frames.begin(); frame != frames.end(); ++frame)
+  {
+    if ((*frame)->getFrametype() != KEYFRAME)
+      continue;
+    Skeleton skeleton = (*frame)->getSkeleton();
+    skeleton.infer3D();
+    tree <BodyPart> partTree = skeleton.getPartTree();
+    for (tree <BodyPart>::iterator bodyPart = partTree.begin(); bodyPart != partTree.end(); ++bodyPart)
+    {
+      BodyJoint *parentJoint = skeleton.getBodyJoint(bodyPart->getParentJoint());
+      BodyJoint *childJoint = skeleton.getBodyJoint(bodyPart->getChildJoint());
+      if (parentJoint->getSpaceLocation() == Point3f(0.0, 0.0, 0.0) || childJoint->getSpaceLocation() == Point3f(0.0, 0.0, 0.0))
+      {
+        FAIL();
+      }
+      Point3f point1 = parentJoint->getSpaceLocation();
+      Point3f point2 = Point3f(childJoint->getSpaceLocation().x, childJoint->getSpaceLocation().y, 0.0);
+      Point3f point3 = childJoint->getSpaceLocation();
+
+      float length12 = PoseHelper::distSquared3d(point1, point2);
+      float length23 = PoseHelper::distSquared3d(point2, point3);
+      float length13 = PoseHelper::distSquared3d(point1, point3);
+
+      EXPECT_NEAR(length13, length12 + length23, 0.999);
+    }
+  }
+
+  Sequence sequence;
+  sequence.setFrames(frames);
+  map <string, float> params;
+
+  sequence.estimateUniformScale(params);
+
+  for (vector <Frame*>::iterator frame = frames.begin(); frame != frames.end(); ++frame)
+  {
+    if ((*frame)->getFrametype() != KEYFRAME)
+      continue;
+    Skeleton skeleton = (*frame)->getSkeleton();
+    skeleton.infer3D();
+    tree <BodyPart> partTree = skeleton.getPartTree();
+    for (tree <BodyPart>::iterator bodyPart = partTree.begin(); bodyPart != partTree.end(); ++bodyPart)
+    {
+      BodyJoint *parentJoint = skeleton.getBodyJoint(bodyPart->getParentJoint());
+      BodyJoint *childJoint = skeleton.getBodyJoint(bodyPart->getChildJoint());
+      if (parentJoint->getSpaceLocation() == Point3f(0.0, 0.0, 0.0) || childJoint->getSpaceLocation() == Point3f(0.0, 0.0, 0.0))
+      {
+        FAIL();
+      }
+      Point3f point1 = parentJoint->getSpaceLocation();
+      Point3f point2 = Point3f(childJoint->getSpaceLocation().x, childJoint->getSpaceLocation().y, 0.0);
+      Point3f point3 = childJoint->getSpaceLocation();
+
+      float length12 = PoseHelper::distSquared3d(point1, point2);
+      float length23 = PoseHelper::distSquared3d(point2, point3);
+      float length13 = PoseHelper::distSquared3d(point1, point3);
+
+      EXPECT_NEAR(length13, length12 + length23, 0.999);
+    }
+  }
+
+  sequence.computeInterpolation(params);
+
+  for (vector <Frame*>::iterator frame = frames.begin(); frame != frames.end(); ++frame)
+  {
+    //TODO(Vitalii Koshura): Remove this after correct work of interpolation
+    if ((*frame)->getFrametype() != KEYFRAME)
+      continue;
+    Skeleton skeleton = (*frame)->getSkeleton();
+    skeleton.infer3D();
+    tree <BodyPart> partTree = skeleton.getPartTree();
+    for (tree <BodyPart>::iterator bodyPart = partTree.begin(); bodyPart != partTree.end(); ++bodyPart)
+    {
+      BodyJoint *parentJoint = skeleton.getBodyJoint(bodyPart->getParentJoint());
+      BodyJoint *childJoint = skeleton.getBodyJoint(bodyPart->getChildJoint());
+      if (parentJoint->getSpaceLocation() == Point3f(0.0, 0.0, 0.0) || childJoint->getSpaceLocation() == Point3f(0.0, 0.0, 0.0))
+      {
+        FAIL();
+      }
+      Point3f point1 = parentJoint->getSpaceLocation();
+      Point3f point2 = Point3f(childJoint->getSpaceLocation().x, childJoint->getSpaceLocation().y, 0.0);
+      Point3f point3 = childJoint->getSpaceLocation();
+
+      float length12 = PoseHelper::distSquared3d(point1, point2);
+      float length23 = PoseHelper::distSquared3d(point2, point3);
+      float length13 = PoseHelper::distSquared3d(point1, point3);
+
+      EXPECT_NEAR(length13, length12 + length23, 0.999);
+    }
+  }
+
+  sequence.estimateUniformScale(params);
+
+  for (vector <Frame*>::iterator frame = frames.begin(); frame != frames.end(); ++frame)
+  {
+    //TODO(Vitalii Koshura): Remove this after correct work of interpolation
+    if ((*frame)->getFrametype() != KEYFRAME)
+      continue;
+    Skeleton skeleton = (*frame)->getSkeleton();
+    skeleton.infer3D();
+    tree <BodyPart> partTree = skeleton.getPartTree();
+    for (tree <BodyPart>::iterator bodyPart = partTree.begin(); bodyPart != partTree.end(); ++bodyPart)
+    {
+      BodyJoint *parentJoint = skeleton.getBodyJoint(bodyPart->getParentJoint());
+      BodyJoint *childJoint = skeleton.getBodyJoint(bodyPart->getChildJoint());
+      if (parentJoint->getSpaceLocation() == Point3f(0.0, 0.0, 0.0) || childJoint->getSpaceLocation() == Point3f(0.0, 0.0, 0.0))
+      {
+        FAIL();
+      }
+      Point3f point1 = parentJoint->getSpaceLocation();
+      Point3f point2 = Point3f(childJoint->getSpaceLocation().x, childJoint->getSpaceLocation().y, 0.0);
+      Point3f point3 = childJoint->getSpaceLocation();
+
+      float length12 = PoseHelper::distSquared3d(point1, point2);
+      float length23 = PoseHelper::distSquared3d(point2, point3);
+      float length13 = PoseHelper::distSquared3d(point1, point3);
+
+      EXPECT_NEAR(length13, length12 + length23, 0.999);
+    }
+  }
+
+}
