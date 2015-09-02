8a2a644f74f802682b5510bee71477927621eef9
mfastovets <mfastovets@users.noreply.github.com>
2015-09-02 02:02:26 +0100

    <Mykyta Fastovets> changed default lockframe threshold.

---
+++ src/spelgui/frameboxwidget.h
@@ -0,0 +1,47 @@
+#ifndef FRAMEBOXWIDGET_H
+#define FRAMEBOXWIDGET_H
+
+#include <QGroupBox>
+#include <QHBoxLayout>
+#include <QPushButton>
+#include <QSlider>
+
+
+class FrameTableWidget;
+
+class FrameBoxWidget : public QGroupBox
+{
+  Q_OBJECT
+public:
+  explicit FrameBoxWidget(QWidget *parent = 0);
+  ~FrameBoxWidget();
+signals:
+  void changeFrametype(int frameNum);
+
+  public slots:
+  void pickFrameEvent(int num);
+  void loadProjectEvent();
+  void closeProjectEvent();
+  private slots:
+  void changeViewerClicked();
+  void frametypeSelectorClicked();
+private:
+  //temp
+  int state = 0;
+  //layouts
+  QHBoxLayout *MainLayout;
+  //type of current frame
+  bool isKeyframe;
+  //number of current frame
+  int num;
+public:
+  //view
+  QPushButton *changeViewer;
+  QPushButton *frametypeSelector;
+  QPushButton *maskEditor;
+  QSlider *itemSkaler;
+  QSlider *maskViewer;
+
+};
+
+#endif // FRAMEBOXWIDGET_H
---
+++ src/spelgui/model_handlers/bodyjointsheaderhandler.cpp
@@ -0,0 +1,33 @@
+#include "bodyjointsheaderhandler.h"
+
+#include <QDomElement>
+#include <QDomDocument>
+#include "projectattr.h"
+
+namespace posegui {
+
+  tree<BodyJoint> BodyJointsHeaderHandler::read(const QDomElement &data){
+    tree<BodyJoint> joints;
+    tree<BodyJoint>::iterator jointsTop = joints.begin();
+    while (!data.isNull()) {
+      BodyJoint joint = jointHandler.read(data);
+      joints.insert(jointsTop, joint);
+      //go to next joint
+      const_cast<QDomElement&>(data) = data.nextSiblingElement();
+    }
+    return joints;
+  }
+
+  QDomElement BodyJointsHeaderHandler::write(const tree<BodyJoint> &model, QDomDocument &controller){
+    QDomElement elem = controller
+      .createElement(BodyJointHeaderAttrs::JOINTS_TAG);
+    tree<BodyJoint>::iterator it = model.begin();
+    while (it != model.end()){
+      QDomElement childElem = jointHandler.write(*it, controller);
+      elem.appendChild(childElem);
+      ++it;
+    }
+    return elem;
+  }
+
+}
