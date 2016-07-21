a17c833a456d277599663ecf6261700c7fa47761
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2016-07-21 03:02:07 +0300

    <Vitalii Koshura>: Fix singleton SpelObject class

--- src/spel/spelObject.cpp
+++ src/spel/spelObject.cpp
@@ -2,14 +2,35 @@
 
 #undef DebugMessage
 
-#ifdef DEBUG
-uint8_t SpelObject::debugLevel = 5;
-#else
-uint8_t SpelObject::debugLevel = 1;
-#endif // DEBUG
+SpelObject & SpelObject::Instance(void) noexcept
+{
+  static SpelObject instance;
+  return instance;
+}
 
 SpelObject::SpelObject(void) noexcept
 {
+#ifdef DEBUG
+  debugLevel = 5;
+#else
+  debugLevel = 1;
+#endif // DEBUG
+}
+
+void SpelObject::_DebugMessage(const std::string & message, const uint8_t importance, const std::string & file, const int line) const noexcept
+{
+  if (debugLevel >= importance)
+    std::cerr << file.c_str() << ":" << line << ":" << message.c_str() << std::endl;
+}
+
+void SpelObject::_setDebugLevel(const uint8_t _debugLevel) noexcept
+{
+  debugLevel = _debugLevel;
+}
+
+uint8_t SpelObject::_getDebugLevel(void) const noexcept
+{
+  return debugLevel;
 }
 
 SpelObject::~SpelObject(void) noexcept
@@ -18,16 +39,15 @@ SpelObject::~SpelObject(void) noexcept
 
 void SpelObject::DebugMessage(const std::string &message, const uint8_t importance, const std::string &file, const int line) noexcept
 {
-  if (SpelObject::debugLevel >= importance)
-    std::cerr << file.c_str() << ":" << line << ":" << message.c_str() << std::endl;
+  SpelObject::Instance()._DebugMessage(message, importance, file, line);
 }
 
 void SpelObject::setDebugLevel(const uint8_t _debugLevel) noexcept
 {
-  SpelObject::debugLevel = _debugLevel;
+  SpelObject::Instance()._setDebugLevel(_debugLevel);
 }
 
 uint8_t SpelObject::getDebugLevel(void) noexcept
 {
-  return SpelObject::debugLevel;
+  return SpelObject::Instance()._getDebugLevel();
 }
--- src/spel/spelObject.hpp
+++ src/spel/spelObject.hpp
@@ -9,13 +9,23 @@
 class SpelObject
 {
 public:
-  SpelObject(void) noexcept;
+  static SpelObject& Instance(void) noexcept;
+
+  SpelObject(const SpelObject&) = delete;
+  void operator=(const SpelObject&) = delete;
+
   ~SpelObject(void) noexcept;
+
   static void DebugMessage(const std::string &message, const uint8_t importance, const std::string &file, const int line) noexcept;
   static void setDebugLevel(const uint8_t _debugLevel) noexcept;
   static uint8_t getDebugLevel(void) noexcept;
 private:
-  static uint8_t debugLevel;
+  SpelObject(void) noexcept;
+  uint8_t debugLevel;
+
+  void _DebugMessage(const std::string &message, const uint8_t importance, const std::string &file, const int line) const noexcept;
+  void _setDebugLevel(const uint8_t _debugLevel) noexcept;
+  uint8_t _getDebugLevel(void) const noexcept;
 };
 
 #define DebugMessage(message, importance) SpelObject::DebugMessage(message, importance, __FILE__, __LINE__)
