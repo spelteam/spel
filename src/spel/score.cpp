84e002b1b41020e73249aa3b6aa204f31bfe249e
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2016-08-14 21:55:06 +0300

    <Vitalii Koshura>: Refactor Score class.

--- src/spel/score.cpp
+++ src/spel/score.cpp
@@ -2,98 +2,140 @@
 // See Score.hpp for more info
 namespace SPEL
 {
-  // default constructor
-  Score::Score(void)
+  Score::Score(void) noexcept
   {
-    score = 0;
-    detName = "";
-    coeff = 1.0f;
-    isWeak = false;
+    m_score = 0;
+    m_detName = "";
+    m_coeff = 1.0f;
+    m_isWeak = false;
   }
 
-  // constructor with params
-  Score::Score(float sc, std::string name, float _coeff)
+  Score::Score(const float score, const std::string & name) noexcept
   {
-    score = sc;
-    detName = name;
-    coeff = _coeff;
-    isWeak = false;
+    m_score = score;
+    m_detName = name;
+    m_coeff = 1.0f;
+    m_isWeak = false;
   }
 
-  Score::~Score(void)
+  Score::Score(const float score, const std::string &name, const float coeff)
+    noexcept
+  {
+    m_score = score;
+    m_detName = name;
+    m_coeff = coeff;
+    m_isWeak = false;
+  }
+
+  Score::Score(const float score, const std::string & name, const float coeff,
+    const bool isWeak) noexcept
+  {
+    m_score = score;
+    m_detName = name;
+    m_coeff = coeff;
+    m_isWeak = isWeak;
+  }
+
+  Score::Score(const Score & score) noexcept
+    : m_score(score.m_score),
+    m_detName(score.m_detName),
+    m_coeff(score.m_coeff),
+    m_isWeak(score.m_isWeak)
   {
   }
 
-  Score &Score::operator=(const Score &s)
+  Score::Score(Score && score) noexcept
+    : m_score(std::move(score.m_score)),
+    m_detName(std::move(score.m_detName)),
+    m_coeff(std::move(score.m_coeff)),
+    m_isWeak(std::move(score.m_isWeak))
   {
-    if (this == &s)
-    {
+  }
+
+  Score::~Score(void) noexcept
+  {
+  }
+
+  Score &Score::operator=(const Score &score) noexcept
+  {
+    if (this == &score)
       return *this;
-    }
-    this->detName = s.getDetName();
-    this->score = s.getScore();
-    this->coeff = s.getCoeff();
-    this->isWeak = s.getIsWeak();
+    m_detName = score.m_detName;
+    m_score = score.m_score;
+    m_coeff = score.m_coeff;
+    m_isWeak = score.m_isWeak;
     return *this;
   }
 
-  float Score::getScore(void) const
+  float Score::getScore(void) const noexcept
   {
-    return score;
+    return m_score;
   }
 
-  void Score::setScore(float _score)
+  void Score::setScore(const float score) noexcept
   {
-    score = _score;
+    m_score = score;
   }
 
-  std::string Score::getDetName(void) const
+  std::string Score::getDetName(void) const noexcept
   {
-    return detName;
+    return m_detName;
   }
 
-  void Score::setDetName(std::string _detName)
+  void Score::setDetName(const std::string &detName) noexcept
   {
-    detName = _detName;
+    m_detName = detName;
   }
 
-  bool Score::operator<(const Score &s) const
+  bool Score::operator<(const Score &score) const noexcept
   {
-    return (this->score * this->coeff < s.getScore() * s.getCoeff());
+    return (m_score * m_coeff < score.m_score * score.m_coeff);
   }
 
-  bool Score::operator>(const Score &s) const
+  bool Score::operator>(const Score &score) const noexcept
   {
-    return (this->score * this->coeff > s.getScore() * s.getCoeff());
+    return (m_score * m_coeff > score.m_score * score.m_coeff);
   }
 
-  bool Score::operator==(const Score &s) const
+  bool Score::operator==(const Score &score) const noexcept
   {
-    return (this->score * this->coeff == s.getScore() * s.getCoeff() && this->detName == s.getDetName());
+    return (m_score * m_coeff == score.m_score * score.m_coeff && 
+      m_detName == score.m_detName);
   }
 
-  bool Score::operator!=(const Score &s) const
+  bool Score::operator!=(const Score &score) const noexcept
   {
-    return !(*this == s);
+    return !(*this == score);
   }
 
-  float Score::getCoeff(void) const
+  Score & Score::operator=(Score && score) noexcept
   {
-    return coeff;
+    m_score = std::move(score.m_score);
+    std::swap(m_detName, score.m_detName);
+    m_coeff = std::move(score.m_coeff);
+    m_isWeak = std::move(score.m_isWeak);
+
+    return *this;
   }
 
-  void Score::setCoeff(float _coeff)
+  float Score::getCoeff(void) const noexcept
   {
-    coeff = _coeff;
+    return m_coeff;
   }
 
-  bool Score::getIsWeak(void) const
+  void Score::setCoeff(const float coeff) noexcept
   {
-    return isWeak;
+    m_coeff = coeff;
   }
-  void Score::setIsWeak(bool _isWeak)
+
+  bool Score::getIsWeak(void) const noexcept
   {
-    isWeak = _isWeak;
+    return m_isWeak;
+  }
+
+  void Score::setIsWeak(const bool isWeak) noexcept
+  {
+    m_isWeak = isWeak;
   }
 
 }
--- src/spel/score.hpp
+++ src/spel/score.hpp
@@ -9,41 +9,114 @@
 
 namespace SPEL
 {
+  /// <summary>
   /// Used to evaluate accuracy of a detection
+  /// </summary>
   class Score
   {
   public:
-    Score(void);
-    Score(float sc, std::string name, float _coeff = 1.0f);
-    virtual ~Score(void);
-    /// copying all fields
-    virtual Score & operator=(const Score &s);
-    /// All this operators perform comparison by score values
-    virtual bool operator<(const Score &s) const;
-    virtual bool operator>(const Score &s) const;
-    virtual bool operator==(const Score &s) const;
-    /// This operator perform comparison by address
-    virtual bool operator!=(const Score &s) const;
-    /// All this functions just give access to the object fields
-    virtual float getScore(void) const;
-    virtual void setScore(float _score);
-    virtual std::string getDetName(void) const;
-    virtual void setDetName(std::string _detName);
-    virtual float getCoeff(void) const;
-    virtual void setCoeff(float _coeff);
-    virtual bool getIsWeak(void) const;
-    virtual void setIsWeak(bool _isWeak);
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Score"/> class.
+    /// </summary>
+    Score(void) noexcept;
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Score"/> class.
+    /// </summary>
+    /// <param name="score">The score.</param>
+    /// <param name="name">The detector name.</param>
+    Score(const float score, const std::string &name) noexcept;
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Score"/> class.
+    /// </summary>
+    /// <param name="score">The score.</param>
+    /// <param name="name">The detector name.</param>
+    /// <param name="coeff">The coefficient.</param>
+    Score(const float score, const std::string &name, const float coeff) 
+      noexcept;
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Score"/> class.
+    /// </summary>
+    /// <param name="score">The score.</param>
+    /// <param name="name">The detector name.</param>
+    /// <param name="coeff">The coefficient.</param>
+    /// <param name="isWeak">The flag of weak score.</param>
+    Score(const float score, const std::string &name, const float coeff, 
+      const bool isWeak) noexcept;
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Score"/> class.
+    /// Copy constructor.
+    /// </summary>
+    /// <param name="score">The score.</param>
+    Score(const Score& score) noexcept;
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Score"/> class.
+    /// Move constructor.
+    /// </summary>
+    /// <param name="score">The score.</param>
+    Score(Score&& score) noexcept;
+    /// <summary>
+    /// Finalizes an instance of the <see cref="Score"/> class.
+    /// </summary>
+    virtual ~Score(void) noexcept;
+    /// <summary>Assignment operator.</summary>
+    /// <param name="score">The score.</param>
+    /// <returns>New score.</returns>
+    Score & operator=(const Score &score) noexcept;
+    /// <summary>Comparison operator.</summary>
+    /// <param name="score">The score.</param>
+    /// <returns>The result of the comparison.</returns>
+    bool operator<(const Score &score) const noexcept;
+    /// <summary>Comparison operator.</summary>
+    /// <param name="score">The score.</param>
+    /// <returns>The result of the comparison.</returns>
+    bool operator>(const Score &score) const noexcept;
+    /// <summary>Comparison operator.</summary>
+    /// <param name="score">The score.</param>
+    /// <returns>The result of the comparison.</returns>
+    bool operator==(const Score &score) const noexcept;
+    /// <summary>Comparison operator.</summary>
+    /// <param name="score">The score.</param>
+    /// <returns>The result of the comparison.</returns>
+    bool operator!=(const Score &score) const noexcept;
+    /// <summary>Move operator.</summary>
+    /// <param name="score">The score.</param>
+    /// <returns>New score.</returns>
+    Score& operator=(Score&& score) noexcept;
+    /// <summary>Gets the score.</summary>
+    /// <returns>The score.</returns>
+    float getScore(void) const noexcept;
+    /// <summary>Sets the score.</summary>
+    /// <param name="score">The score.</param>
+    void setScore(const float score) noexcept;
+    /// <summary>Gets the name of the detector.</summary>
+    /// <returns>The name of the detector.</returns>
+    std::string getDetName(void) const noexcept;
+    /// <summary>Sets the name of the detector.</summary>
+    /// <param name="detName">Name of the detector.</param>
+    void setDetName(const std::string &detName) noexcept;
+    /// <summary>Gets the coefficient.</summary>
+    /// <returns>The coefficient.</returns>
+    float getCoeff(void) const noexcept;
+    /// <summary>Sets the coefficient.</summary>
+    /// <param name="coeff">The coefficient.</param>
+    void setCoeff(const float coeff) noexcept;
+    /// <summary>Gets the flag of the weak score.</summary>
+    /// <returns>The flag of the weak score.</returns>
+    bool getIsWeak(void) const noexcept;
+    /// <summary>Sets the flag of the weak score.</summary>
+    /// <param name="isWeak">The flag of the weak score.</param>
+    void setIsWeak(const bool isWeak) noexcept;
 
-  private:
-    /// detection score
-    float score;
-    /// detector name, name of the algorithm that generate the evaluation
-    std::string detName;
-    float coeff;
-    /// signify label is from a badly localised part i.e. all very weak or all very similar detection scores
-    bool isWeak;
+  private:    
+    /// <summary>The score.</summary>
+    float m_score;    
+    /// <summary>The detector name.</summary>
+    std::string m_detName;    
+    /// <summary>The coefficient.</summary>
+    float m_coeff;    
+    /// <summary>The flag of the weak score.</summary>
+    bool m_isWeak;
   };
-
 }
 
 #endif  // _SCORE_HPP_
