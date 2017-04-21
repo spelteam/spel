e794e77dfce5381de7ce78bd9c8d98dc86367480
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2017-04-22 00:46:20 +0300

    Improve build. Add qt5 dependency
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- linux_install_dependencies.sh
+++ linux_install_dependencies.sh
@@ -10,7 +10,9 @@ wget https://github.com/opengm/opengm/archive/master.zip
 unzip master.zip > /dev/null 2>&1
 mkdir build
 cd build
-cmake -DBUILD_EXAMPLES=OFF -DBUILD_TUTORIALS=OFF ../opengm-master && make -j2 && sudo make install
+cmake -DBUILD_EXAMPLES=OFF -DBUILD_TUTORIALS=OFF -dBUILD_TESTING=OFF ../opengm-master
+#make -j2
+sudo make install
 cd ../../
 
 # OpenCV
@@ -24,17 +26,20 @@ unzip 3.2.0.zip > /dev/null 2>&1
 rm 3.2.0.zip
 mkdir build
 cd build
-cmake -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib-3.2.0/modules -DBUILD_opencv_apps=OFF -DBUILD_DOCS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DBUILD_FAT_JAVA_LIB=OFF ../opencv-3.2.0 && make -j2 && sudo make install
+cmake -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib-3.2.0/modules -DBUILD_opencv_apps=OFF -DBUILD_DOCS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DBUILD_FAT_JAVA_LIB=OFF ../opencv-3.2.0
+make -j2
+sudo make install
 cd ../../
 
 # Eigen
 mkdir eigen
 cd eigen
 wget http://bitbucket.org/eigen/eigen/get/3.2.10.zip
-unzip 3.2.10.zip
+unzip 3.2.10.zip > /dev/null 2>&1
 mkdir build
 cd build
-cmake ../eigen-eigen-b9cd8366d4e8 && sudo make install
+cmake ../eigen-eigen-b9cd8366d4e8
+sudo make install
 cd ../../
 
 cd ../
