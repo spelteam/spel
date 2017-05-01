d1669bf76acf6bbe0e57194b6fd2b99a5c852642
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2017-05-01 06:06:58 +0300

    appveyor: download OpenGM
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- windows_build.bat
+++ windows_build.bat
@@ -5,6 +5,20 @@ set platform=%2
 
 call "%VS120COMNTOOLS%\vsvars32.bat"
 
+cd ../
+mkdir dependencies
+cd dependencies
+
+REM OpenGM
+mkdir OpenGM
+cd OpenGM
+curl -o 'master.zip' 'https://github.com/opengm/opengm/archive/master.zip'
+7z x master.zip
+REM powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://github.com/opengm/opengm/archive/master.zip','C:\dependencies\OpenGM')
+cd ../
+
+cd ../spel
+
 mkdir build
 cd build
 cmake ../src/
