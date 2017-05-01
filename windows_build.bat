c2392da97ab3746b6534b3aa4782a0840f566a79
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2017-05-01 22:44:01 +0300

    appveyor: fix command line
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- windows_build.bat
+++ windows_build.bat
@@ -31,7 +31,7 @@ cd build
 cmake -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib-3.2.0/modules -DBUILD_opencv_apps=OFF -DBUILD_DOCS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DBUILD_FAT_JAVA_LIB=OFF ../opencv-3.2.0
 REM devenv OpenCV.sln /Build "%configuration%|%platform%"
 dir
-devenv C:\projects\dependencies\opencv\build\OpenCV.sln /Build "Debug|x64"
+devenv C:\projects\dependencies\opencv\build\OpenCV.sln /build "Debug|x64"
 cd ../../
 
 cd ../spel
