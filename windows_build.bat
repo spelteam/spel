77f4ef9ee1bae9621417e5deedf0ee4718bfa177
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2017-05-01 22:08:41 +0300

    appveyor: add opencv
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- windows_build.bat
+++ windows_build.bat
@@ -16,10 +16,26 @@ powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://
 7z x master.zip
 cd ../
 
+REM OpenCV
+mkdir opencv
+cd opencv
+powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://github.com/opencv/opencv/archive/3.2.0.zip','C:\projects\dependencies\opencv\3.2.0.zip')
+7z x 3.2.0.zip
+rm 3.2.0.zip
+powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://github.com/opencv/opencv_contrib/archive/3.2.0.zip','C:\projects\dependencies\opencv\3.2.0.zip')
+wget https://github.com/opencv/opencv_contrib/archive/3.2.0.zip
+7z x 3.2.0.zip
+rm 3.2.0.zip
+mkdir build
+cd build
+cmake -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib-3.2.0/modules -DBUILD_opencv_apps=OFF -DBUILD_DOCS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DBUILD_FAT_JAVA_LIB=OFF ../opencv-3.2.0
+devenv OpenCV.sln /Build "%configuration%|%platform%"
+cd ../../
+
 cd ../spel
 
 mkdir build
 cd build
-cmake ../src/ -DOpenGM_INCLUDE_DIR='C:\projects\dependencies\OpenGM\opengm-master\include'
+cmake ../src/ -DOpenGM_INCLUDE_DIR='C:\projects\dependencies\OpenGM\opengm-master\include' -DOpenCV_DIR='C:\projects\dependencies\opencv\build'
 
 REM devenv %solution_name% /Build "%configuration%|%platform%"
