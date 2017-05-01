c8824a2d98598fff6b611ee715ad25885ead7479
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2017-05-02 02:40:20 +0300

    appveyor: add Eigen3, GTest, timyxml2
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- windows_build.bat
+++ windows_build.bat
@@ -20,7 +20,6 @@ powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://
 7z x 3.2.0.zip
 rm 3.2.0.zip
 powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://github.com/opencv/opencv_contrib/archive/3.2.0.zip','C:\projects\dependencies\opencv\3.2.0.zip')
-wget https://github.com/opencv/opencv_contrib/archive/3.2.0.zip
 7z x 3.2.0.zip
 rm 3.2.0.zip
 mkdir build
@@ -29,10 +28,34 @@ cmake -DCMAKE_GENERATOR_PLATFORM=x64 -DOPENCV_EXTRA_MODULES_PATH=../opencv_contr
 devenv OpenCV.sln /build "Debug|x64"
 cd ../../
 
+REM Eigen
+mkdir eigen
+cd eigen
+powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('http://bitbucket.org/eigen/eigen/get/3.2.10.zip','C:\projects\dependencies\eigen\3.2.10.zip')
+7z x unzip 3.2.10.zip
+cmake ../eigen-eigen-b9cd8366d4e8
+cd ../
+
+REM Google Test
+mkdir gtest
+cd gtest
+powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://github.com/google/googletest/archive/release-1.8.0.zip','C:\projects\dependencies\gtest\release-1.8.0.zip')
+7z x release-1.8.0.zip
+mv googletest-release-1.8.0/googletest/ ../../../thirdparty/
+cd ../
+
+REM TinyXml2
+mkdir tinyxml
+cd tinyxml
+powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://github.com/leethomason/tinyxml2/archive/4.0.1.zip','C:\projects\dependencies\tinyxml\4.0.1.zip')
+7z x 4.0.1.zip
+mv tinyxml2-4.0.1/ ../../../thirdparty/tinyxml2
+cd ../
+
 cd ../spel
 
 mkdir build
 cd build
-cmake ../src/ -DCMAKE_GENERATOR_PLATFORM=x64 -DOpenGM_INCLUDE_DIR='C:\projects\dependencies\OpenGM\opengm-master\include' -DOpenCV_DIR='C:\projects\dependencies\opencv\build'
+cmake ../src/ -DCMAKE_GENERATOR_PLATFORM=x64 -DOpenGM_INCLUDE_DIR='C:\projects\dependencies\OpenGM\opengm-master\include' -DOpenCV_DIR='C:\projects\dependencies\opencv\build' -DEigen3_INCLUDE_DIR='C:\projects\dependencies\eigen\eigen-eigen-b9cd8366d4e8'
 
 REM devenv %solution_name% /Build "%configuration%|%platform%"
