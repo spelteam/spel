e302fcb84c3459906290da4c947e24efce9f6151
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2017-05-02 03:38:31 +0300

    appveyor: add hdf5 and qt
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- windows_build.bat
+++ windows_build.bat
@@ -36,6 +36,13 @@ powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('http://b
 cmake ../eigen-eigen-b9cd8366d4e8
 cd ../
 
+REM HDF5
+mkdir hdf5
+cd hdf5
+powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.14/bin/windows/extra/hdf5-1.8.14-win64-vs2013-shared.zip','C:\projects\dependencies\hdf5\hdf5-1.8.14-win64-vs2013-shared.zip')
+7z x hdf5-1.8.14-win64-vs2013-shared.zip
+cd ../
+
 REM Google Test
 mkdir gtest
 cd gtest
@@ -56,6 +63,5 @@ cd ../spel
 
 mkdir build
 cd build
-cmake ../src/ -DCMAKE_GENERATOR_PLATFORM=x64 -DOpenGM_INCLUDE_DIR='C:\projects\dependencies\OpenGM\opengm-master\include' -DOpenCV_DIR='C:\projects\dependencies\opencv\build' -DEigen3_INCLUDE_DIR='C:\projects\dependencies\eigen\eigen-eigen-b9cd8366d4e8'
-
-REM devenv %solution_name% /Build "%configuration%|%platform%"
+cmake ../src/ -DCMAKE_GENERATOR_PLATFORM=x64 -DOpenGM_INCLUDE_DIR='C:\projects\dependencies\OpenGM\opengm-master\include' -DOpenCV_DIR='C:\projects\dependencies\opencv\build' -DEigen3_INCLUDE_DIR='C:\projects\dependencies\eigen\eigen-eigen-b9cd8366d4e8' -DHDF5_HL_IMPORT_LIB='C:\projects\dependencies\hdf5\HDF5-1.8.14-win64\lib\hdf5_hl.lib' -DHDF5_IMPORT_LIB='C:\projects\dependencies\hdf5\HDF5-1.8.14-win64\lib\hdf5.lib'
+-DHDF5_INCLUDE_DIR='C:\projects\dependencies\hdf5\HDF5-1.8.14-win64\include' -DQt5Core_DIR='C:\Qt\5.8\msvc2013_64\lib\cmake\Qt5Core' -DQt5Gui_DIR='C:\Qt\5.8\msvc2013_64\lib\cmake\Qt5Gui' -DQt5Network_DIR='C:\Qt\5.8\msvc2013_64\lib\cmake\Qt5Network' -DQt5OpenGL_DIR='C:\Qt\5.8\msvc2013_64\lib\cmake\Qt5OpenGL' -DQt5Widgets_DIR='C:\Qt\5.8\msvc2013_64\lib\cmake\Qt5Widgets' -DQt5Xml_DIR='C:\Qt\5.8\msvc2013_64\lib\cmake\Qt5Xml' -DQt5XmlPatterns_DIR='C:\Qt\5.8\msvc2013_64\lib\cmake\Qt5XmlPatterns'
